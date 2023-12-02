921e75681d8a03e36ed4dddd1df9a77309bbfb6a
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2023-12-02 19:03:13 +0100

    `Statue`: hide inventory bar on enter
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- Assets/spawn/Statue.cs
+++ Assets/spawn/Statue.cs
@@ -10,10 +10,12 @@ public class Statue : MonoBehaviour
     {
         if(!other.CompareTag("Player")) { return; }
         StatueMenu.SetActive(true);
+        InventoryBar.Instance.gameObject.SetActive(false);
     }
     private void OnTriggerExit(Collider other)
     {
         if (!other.CompareTag("Player")) { return; }
         StatueMenu.SetActive(false);
+        InventoryBar.Instance.gameObject.SetActive(true);
     }
 }
